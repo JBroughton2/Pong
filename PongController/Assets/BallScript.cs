b5b76848ef470bc0184374ea8f13d8d3d2a365a8
joe <jodeph.broughton@gmail.com>
2020-01-28 16:37:28 +0000

    uploaded project

---
+++ PongController/Assets/BallScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public int initialRot;
+    public Vector2 startingSpeed;
+    public bool destroyed;
+    public GameManager gm;
+    public GameObject gameMaster;
+    public Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        gm = gameMaster.GetComponent<GameManager>();
+        moveBall();
+    }
+
+    void Update()
+    {
+
+    }
+    public void moveBall()
+    {
+        ChooseStart();
+        if (initialRot == 180 || initialRot == 0)
+        {
+            ChooseStart();
+        }
+        else
+        {
+            rb.AddForce(startingSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Win"))
+        {
+            destroyed = true;
+            Destroy(this.gameObject);
+        }
+    }
+
+    void ChooseStart()
+    {
+        initialRot = Random.Range(0,360);
+    }
+}
---
+++ PongController/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ball;
+    public Vector3 startPosition;
+    public bool destroyed;
+    public BallScript bs;
+
+    void Start()
+    {
+        bs = ball.GetComponent<BallScript>();
+        
+    }
+
+    void Update()
+    {
+        spawnBall();
+    }
+
+    void spawnBall()
+    {
+        if (bs.destroyed)
+        {
+            Instantiate(ball, startPosition, Quaternion.identity);
+            bs.rb = ball.GetComponent<Rigidbody2D>();
+            bs.moveBall();
+            bs.destroyed = false;
+        }
+    }
+
+}
